b534e6f8c834e2db5dc7bb281070a5a27cf89ddf
diraizelet <varunsuren2003@gmail.com>
2024-03-05 11:50:27 +0530

    Made basic ui elements and added a mobile screen scaling

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import './UsersList.css';
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UsersList = props => {
 
 
      if(props.items.length === 0){
         return (<div className="center">
+            <Card>
             <h2>No Users Found</h2>
+            </Card>
         </div>);
      }
      return <ul className="user-list">
