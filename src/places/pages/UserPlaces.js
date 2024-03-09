2aaa01f74c33a7bfa7287217f4d71cfbdeaccb2a
diraizelet <varunsuren2003@gmail.com>
2024-03-09 21:07:07 +0530

    71 more of the edit, add and no place cases are handled

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,41 +1,41 @@
-import React from "react";
-
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import PlaceList from "../components/PlaceList";
 
-const DUMMY_PLACES = [ 
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'Famous SkyScraper',
-        imageUrl: 'https://imgs.search.brave.com/KNIgN3TVstsZcgbMM_3uBkZlqGv-eISTfZYggZD9Ro8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMjQvMTUwOTI0/LTEzOC03QTU0MUJD/MC9FbXBpcmUtU3Rh/dGUtQnVpbGRpbmct/TmV3LVlvcmstQ2l0/eS5qcGc_dz04MDAm/aD00NTAmYz1jcm9w',
-        address: '350 Fifth Avenue, New York, NY 10118, USA',
-        location:{
-            lat:  40.748817,
-            lng:-73.985428
-        },
-        creator: 'u1'
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'Famous SkyScraper',
-        imageUrl: 'https://imgs.search.brave.com/KNIgN3TVstsZcgbMM_3uBkZlqGv-eISTfZYggZD9Ro8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMjQvMTUwOTI0/LTEzOC03QTU0MUJD/MC9FbXBpcmUtU3Rh/dGUtQnVpbGRpbmct/TmV3LVlvcmstQ2l0/eS5qcGc_dz04MDAm/aD00NTAmYz1jcm9w',
-        address: '350 Fifth Avenue, New York, NY 10118, USA',
-        location:{
-            lat:  40.748817,
-            lng:-73.985428
-        },
-        creator: 'u2'
-    }
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Emp. State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
 ];
 
-const UserPlaces = props =>{
-
-    const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-    return <PlaceList items={loadedPlaces}/>;
-
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
