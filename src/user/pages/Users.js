95b6013e572834579cd02e169af1504681430ade
diraizelet <varunsuren2003@gmail.com>
2024-03-11 18:22:52 +0530

    corrected and removed the dot and look of the user panel

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,7 +4,8 @@ import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS =[
-    {id: "u1", name: 'diraizelet' ,
+    {id: "u1", 
+    name: 'diraizelet' ,
     image: 'https://imgs.search.brave.com/dwGEyWpnuuGAV64kJp-mvnhWjKEh5pnaEjw_KmVRmaQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wbGF5/LWxoLmdvb2dsZXVz/ZXJjb250ZW50LmNv/bS9lLWdXRFoxLXhv/SEhReXhIOGpvbVpR/ZFBBcVZIMnhLSjRz/N1B6Rl9DWXNGd1BF/ZTN3OUhZTVh2VWZx/dVpOMlhrdWFnPXc1/MjYtaDI5Ni1ydw', 
     place: 3}
   ];
